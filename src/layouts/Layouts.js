71afd29d58ef3984b039f576a2214d247091ceab
Osama Yousuf <usama.yousuf@weuno.co>
2023-01-30 21:05:38 +0500

    redux things changed

--- src/layouts/Layouts.js
+++ src/layouts/Layouts.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { Root, Auth } from "../routing";
 import { asynchronouslyGetFromLocal, setInLocalStorage } from '../utils/helperFunctions';
@@ -12,62 +12,102 @@ const Layouts = () => {
   const selectLogin = useAppSelector(selectIsLogin);
   const isLoading = useAppSelector(selectIsLoading);
 
+  // useEffect(() => {
+  //   if (selectLayout?.token) {
+  //     const { token } = selectLayout;
+  //     setInLocalStorage("APP_TOKEN", token);
+  //   }
+  // }, [selectLayout]);
+
+  // const checkIsTokenInLocal = async () => {
+  //   let APP_TOKEN = await asynchronouslyGetFromLocal("APP_TOKEN");
+  //   if (APP_TOKEN) {
+  //     return;
+  //   }
+  //   else {
+  //     dispatch(getAuthToken());
+  //   }
+  // };
+
+
+  // useEffect(() => {
+  //   checkIsTokenInLocal();
+  // }, []);
+
+  // const checkAuth = async () => {
+  //   const user = await asynchronouslyGetFromLocal("USER_ID");
+  //   if (user) {
+  //     dispatch(SelectAuth(user));
+  //   } else {
+  //     dispatch(SelectAuth(null));
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   checkAuth();
+  // }, []);
+
+
   useEffect(() => {
     if (selectLayout?.token) {
-      const { token } = selectLayout;
-      setInLocalStorage("APP_TOKEN", token);
+      setInLocalStorage("APP_TOKEN", selectLayout?.token);
     }
   }, [selectLayout]);
 
-  const checkIsTokenInLocal = async () => {
-    let APP_TOKEN = await asynchronouslyGetFromLocal("APP_TOKEN");
-    if (APP_TOKEN) {
-      return;
-    }
-    else {
-      dispatch(getAuthToken());
-    }
-  };
-
-
   useEffect(() => {
+    
+    const checkIsTokenInLocal = async () => {
+      let APP_TOKEN = await asynchronouslyGetFromLocal("APP_TOKEN");
+      if (!APP_TOKEN) {
+        dispatch(getAuthToken());
+      }
+    };
+    
+    const checkAuth = async () => {
+      const user = await asynchronouslyGetFromLocal("USER_ID");
+      dispatch(SelectAuth(user ? user : null));
+    };
+    
     checkIsTokenInLocal();
-  }, []);
-
-  const checkAuth = async () => {
-    const user = await asynchronouslyGetFromLocal("USER_ID");
-    if (user) {
-      dispatch(SelectAuth(user));
-    } else {
-      dispatch(SelectAuth(null));
-    }
-  };
-
-  useEffect(() => {
     checkAuth();
-  }, []);
+  }, [dispatch]);
 
-  const renderRouting = (isAuth) => {
-    console.log(isAuth);
-    switch (isAuth) {
+  // const renderRouting = (isAuth) => {
+  //   console.log(isAuth);
+  //   switch (isAuth) {
+  //     case true:
+  //       return <Root />;
+  //     case false:
+  //       return <Auth />;
+  //     default:
+  //       return <Auth />;
+  //   }
+  // };
+
+  // return (
+  //   <>
+  //     {isLoading !== null && isLoading ? (
+  //       <div className="loading"></div>
+  //     ) : (
+  //       <div>{renderRouting(selectLogin)}</div>
+  //     )}
+  //   </>
+  // )
+
+  const renderRouting = useMemo(() => {
+    switch (selectLogin) {
       case true:
         return <Root />;
-      case false:
-        return <Auth />;
       default:
         return <Auth />;
     }
-  };
+  }, [selectLogin]);
 
-  return (
-    <>
-      {isLoading !== null && isLoading ? (
-        <div className="loading"></div>
-      ) : (
-        <div>{renderRouting(selectLogin)}</div>
-      )}
-    </>
-  )
+  return isLoading === null || isLoading ? (
+    <div className="loading"></div>
+  ) : (
+    <div>{renderRouting}</div>
+  );
 }
 
 export default Layouts
--- src/routing/index.js
+++ src/routing/index.js
@@ -6,25 +6,85 @@ import Login from "../pages/login/Login";
 import Signup from "../pages/signup/Signup";
 import { routes } from "./routes";
 
+// function Auth() {
+//     return (
+//         <Fragment>
+//             <Router>
+//                 <Routes>
+//                     <Route exact path="/login" element={<Login />} />
+//                     <Route exact path="/signup" element={<Signup />} />
+//                     <Route exact path="/" element={<App />} />
+//                     <Route
+//                         path="*"
+//                         element={<Navigate to="/login" from="/*" component={Login} replace />}
+//                     />
+//                 </Routes>
+//             </Router>
+//         </Fragment>
+//     );
+// }
+
+// function Root(props) {
+//     return (
+//         <Router>
+//             <Routes>
+//                 {routes.map((route, index) => (
+//                     <Route
+//                         key={index}
+//                         exact={route.exact}
+//                         path={route.path}
+//                         element={route.component}
+//                     />
+//                 ))}
+//                 <Route
+//                     path="/"
+//                     element={<Navigate to="/home" from="/" component={Home} />}
+//                 />
+//                 <Route
+//                     path="/login"
+//                     element={<Navigate to="/home" from="/login" component={Home} />}
+//                 />
+//                 <Route
+//                     path="/signup"
+//                     element={<Navigate to="/home" from="/signup" component={Home} />}
+//                 />
+//                 <Route
+//                     path="*"
+//                     render={() => (
+//                         <div className="w-100 h-100">
+//                             <h1>404 - Page not found</h1>
+//                         </div>
+//                     )}
+//                 />
+//             </Routes>
+//         </Router>
+//     )
+// }
+
 function Auth() {
     return (
-        <Fragment>
-            <Router>
-                <Routes>
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/signup" element={<Signup />} />
-                    <Route exact path="/" element={<App />} />
-                    <Route
-                        path="*"
-                        element={<Navigate to="/login" from="/*" component={Login} replace />}
-                    />
-                </Routes>
-            </Router>
-        </Fragment>
+        <Router>
+            <Routes>
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route exact path="/" element={<App />} />
+                <Route
+                    path="*"
+                    element={<Navigate to="/login" from="/*" component={Login} replace />}
+                />
+            </Routes>
+        </Router>
     );
 }
 
+
 function Root(props) {
+    const redirectRoutes = [
+        { from: '/', to: '/home' },
+        { from: '/login', to: '/home' },
+        { from: '/signup', to: '/home' },
+    ];
+
     return (
         <Router>
             <Routes>
@@ -36,18 +96,12 @@ function Root(props) {
                         element={route.component}
                     />
                 ))}
-                <Route
-                    path="/"
-                    element={<Navigate to="/home" from="/" component={Home} />}
-                />
-                <Route
-                    path="/login"
-                    element={<Navigate to="/home" from="/login" component={Home} />}
-                />
-                <Route
-                    path="/signup"
-                    element={<Navigate to="/home" from="/signup" component={Home} />}
-                />
+                {redirectRoutes.map(({ from, to }) => (
+                    <Route
+                        path={from}
+                        render={() => <Navigate to={to} component={Home} />}
+                    />
+                ))}
                 <Route
                     path="*"
                     render={() => (
@@ -58,6 +112,7 @@ function Root(props) {
                 />
             </Routes>
         </Router>
-    )
+    );
 }
+
 export { Auth, Root };
